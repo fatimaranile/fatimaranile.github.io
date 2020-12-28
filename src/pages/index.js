7ccb5002972a4ee714f659ce3dc26a63fac1ab88
Pat Ranile <fatimaranile@gmail.com>
2020-12-28 22:13:31 +0800

    Update contacts component. Adjust header to start and navigation to end.

--- src/pages/index.js
+++ src/pages/index.js
@@ -149,31 +149,6 @@ class Index extends React.Component {
               </ul>
             </footer>
           </section>
-
-          <section id="cta" className="main special">
-            <header className="major">
-              <h2>Congue imperdiet</h2>
-              <p>
-                Donec imperdiet consequat consequat. Suspendisse feugiat congue
-                <br />
-                posuere. Nulla massa urna, fermentum eget quam aliquet.
-              </p>
-            </header>
-            <footer className="major">
-              <ul className="actions">
-                <li>
-                  <Link to="/generic" className="button special">
-                    Get Started
-                  </Link>
-                </li>
-                <li>
-                  <Link to="/generic" className="button">
-                    Learn More
-                  </Link>
-                </li>
-              </ul>
-            </footer>
-          </section>
         </div>
       </Layout>
     )
