828398735bd0b1e1e67d4239fdc5da30de31370e
Pat Ranile <fatimaranile@gmail.com>
2020-12-28 22:29:22 +0800

    Change theme color

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,8 +12,8 @@ module.exports = {
         name: 'gatsby-starter-default',
         short_name: 'starter',
         start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
+        background_color: '#d9843e',
+        theme_color: '#d9843e',
         display: 'minimal-ui',
         icon: 'src/assets/images/website-icon.png', // This path is relative to the root of the site.
       },
